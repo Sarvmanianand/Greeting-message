6799ae2bab713b53e56136afe63c40ab1a9cd738
Sarvmanianand <anandsarvmani1997@gmail.com>
2020-09-03 23:04:33 +0530

    initial

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+let curDate = new Date(2020 , 8,8,22);
+curDate = curDate.getHours();
+let greeting ="";
+if(curDate >= 1 && curDate<12){
+greeting = "Good Morning";
+}
+else if(curDate>=12 && curDate<17){
+greeting = "Good Evening";
+}
+else {
+  greeting = "goodnight";
+
+}
+
+ReactDOM.render(
+  <>
+  <h1>Hello Sir,  {greeting} </h1>
+  </>
+  ,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
