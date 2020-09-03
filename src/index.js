6b6b5704b35153c8148e5b1990073290a64e4651
Sarvmanianand <anandsarvmani1997@gmail.com>
2020-09-03 23:49:13 +0530

    Added logic for display colour according to time and css style

--- src/index.js
+++ src/index.js
@@ -4,23 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-let curDate = new Date(2020 , 8,8,22);
+let curDate = new Date(2020 , 8,8,14);
 curDate = curDate.getHours();
 let greeting ="";
+const cssStyle = { };
 if(curDate >= 1 && curDate<12){
 greeting = "Good Morning";
+cssStyle.color = 'green';
 }
 else if(curDate>=12 && curDate<17){
 greeting = "Good Evening";
+cssStyle.color = 'orange';
 }
 else {
   greeting = "goodnight";
-
+   cssStyle.color = 'black';
 }
-
 ReactDOM.render(
   <>
-  <h1>Hello Friends,  {greeting} </h1>
+  <div>
+  <h1>Hello Sir, <span style= {cssStyle}> {greeting} </span></h1>
+  </div>
   </>
   ,
   document.getElementById('root')
